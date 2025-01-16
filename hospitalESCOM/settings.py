3f1545e0cc32dc5a4f4aed2068d90be01e573ef4
adrianvm <caradvm@gmail.com>
2025-01-16 15:27:21 -0600

    Final

--- hospitalESCOM/settings.py
+++ hospitalESCOM/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-@1d_xcuenmr1v%3)r1jqmk3v701by_5*an7b31u%w28=&06dve
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
